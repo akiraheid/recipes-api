e9fead64c429fb8153006f0e5bea0d07b982cc76
Akira Heid <akira710@protonmail.com>
2020-08-19 22:31:43 -0400

    add test DELETE /user/id authenticated but not as id

--- test/DELETE.user.js
+++ test/DELETE.user.js
@@ -33,6 +33,37 @@ describe('DELETE /user/:id authenticated', () => {
 	})
 })
 
+describe('DELETE /user/:id authenticated delete another user', () => {
+	let agent = undefined
+	const otherUsername = 'otheruser'
+
+	beforeEach(async () => {
+		agent = util.createAgent()
+
+		await util.deleteUser(testUsername)
+
+		// Create user and store auth token in agent
+		const res = await agent.post('/user')
+			.send({ username: testUsername, password: testUsername })
+
+		expect(res).to.have.status(200)
+
+		// Create other user
+		await util.createUser(otherUsername)
+	})
+
+	afterEach(async () => {
+		agent.close()
+		await util.deleteUser(otherUsername)
+		await util.deleteUser(testUsername)
+	})
+
+	it('returns 401', async () => {
+		const res = await agent.delete(`/user/${otherUsername}`)
+		expect(res).to.have.status(401)
+	})
+})
+
 describe('DELETE /user/:id unauthenticated', () => {
 	it('returns 401', async () => {
 		const nonExistentUsername = 'Idontexistnaenae'
